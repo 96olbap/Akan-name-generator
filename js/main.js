1b80d19f1002f4f39574bc745f0c58db56c546b2
96olbap <alvinwanjala@gmail.com>
2021-12-14 15:22:00 +0300

    This commit will add validation to the date inputs in the form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+let btnValidate = document.querySelector('button');
+let result = document.querySelector('p');
 
+let inputMonth = document.querySelector('#month');
+let inputDay = document.querySelector('#day');
+let inputYear  = document.querySelector('#year');
+
+btnValidate.addEventListener('click', () =>{
+  let month = inputMonth.value;
+  let day = inputDay.value;
+  let year = inputYear.value;
+  result.innerText = moment('${month}/${day}/${year}','MM/DD/YYYY', true).isValid();
+});
