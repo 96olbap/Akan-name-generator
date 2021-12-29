0d07be0f331543bfbcfe9f878d2bf2591192723b
96olbap <alvinwanjala@gmail.com>
2021-12-29 21:03:50 +0300

    This commit will add the function to calculate the specific day someone was born

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ function validateDate(){
     alert('please proceed to enter your gender');
   }
 }
+
+function generateName(){
+  var gender = document.querySelector('input[type = "radio"]:checked').value;
+  var female = getElementById('female');
+  var male = getElementById('male')
+  var birthDay = function(month, day, year) {
+    var month = document.getElementById('month').value;
+    var day = document.getElementById('day').value;
+    var year = document.getElementById('year').value;
+
+    var yy = parseInt(year.slice(-2));
+    var mm = parseInt(month);
+    var dd = parseInt(day);
+    var cc = parseInt(year.slice(0,2));
+    var date = (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd) %7
+    var dDay = Math.ceil(date);
+  }
+
+
+}
