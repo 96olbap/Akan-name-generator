daf614db9841d12681e1b9b843a81c9d9a2cc1d3
96olbap <alvinwanjala@gmail.com>
2022-01-10 17:57:59 +0300

    corrected text alignment

--- js/main.js
+++ js/main.js
@@ -8,6 +8,18 @@ function validateDate(){
   }else if (dayValid <= 0 || dayValid > 31) {
     alert('please enter a valid day');
   }else {
+    var birthDay = function(month, day, year) {
+      var month = document.getElementById('month').value;
+      var day = document.getElementById('day').value;
+      var year = document.getElementById('year').value;
+
+      var yy = parseInt(year.toString().slice(-2));
+      var mm = parseInt(month);
+      var dd = parseInt(day);
+      var cc = parseInt(year.toString().slice(0,2));
+      var date = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) %7
+      var dDay = Math.ceil(date);
+    }
     alert('please proceed to enter your gender');
   }
 }
@@ -16,97 +28,87 @@ function generateName(){
   var gender = document.querySelector('input[type = "radio"]:checked').value;
   var female = getElementById('female');
   var male = getElementById('male')
-  var birthDay = function(month, day, year) {
-    var month = document.getElementById('month').value;
-    var day = document.getElementById('day').value;
-    var year = document.getElementById('year').value;
-
-    var yy = parseInt(year.slice(-2));
-    var mm = parseInt(month);
-    var dd = parseInt(day);
-    var cc = parseInt(year.slice(0,2));
-    var date = (((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd) %7
-    var dDay = Math.ceil(date);
-
-    function checkBirthDay(){
-      var bDay = dDay;
-      if (bDay == 0){
-        return "Sunday"
-      }
-      else if (bDay == 1) {
-        return "Monday"
-      }
-      else if (bDay == 2) {
-        return "Tuesday"
-      }
-      else if (bDay == 3) {
-        return "Wednesday"
-      }
-      else if (bDay == 4) {
-        return "Thursday"
-      }
-      else if (bDay == 5) {
-        return "Friday"
-      }
-      else if (bDay == 6) {
-        return "Saturday"
-      }
+  function checkBirthDay(){
+    var bDay = dDay;
+    if (bDay == 0){
+      return "Sunday"
     }
-
-    if (female.checked == true){
-      var femaleAkanName;
-      switch(dDay){
-        case 0:
-            femaleAkanName = 'Akosua';
-            break;
-        case 1:
-            femaleAkanName = "Adwoa";
-            break;
-        case 2:
-            femaleAkanName = "Abeena";
-            break;
-        case 3:
-            femaleAkanName = "Akua";
-            break;
-        case 4:
-            femaleAkanName = "Yaa";
-            break;
-        case 5:
-            femaleAkanName = "Afua";
-        case 6:
-            femaleAkanName = "Ama";
-            break;
-      }
-      //alert(femaleAkanName);
-      document.getElementById("output").innerHTML = "Your Akan Name is "+" "+femaleAkanName;
+    else if (bDay == 1) {
+      return "Monday"
     }
+    else if (bDay == 2) {
+      return "Tuesday"
+    }
+    else if (bDay == 3) {
+      return "Wednesday"
+    }
+    else if (bDay == 4) {
+      return "Thursday"
+    }
+    else if (bDay == 5) {
+      return "Friday"
+    }
+    else if (bDay == 6) {
+      return "Saturday"
+    }
+  }
+  if (female.checked == true){
+    var femaleAkanName;
+    switch(dDay){
+      case 0:
+          femaleAkanName = 'Akosua';
+          break;
+      case 1:
+          femaleAkanName = "Adwoa";
+          break;
+      case 2:
+          femaleAkanName = "Abeena";
+          break;
+      case 3:
+          femaleAkanName = "Akua";
+          break;
+      case 4:
+          femaleAkanName = "Yaa";
+          break;
+      case 5:
+          femaleAkanName = "Afua";
+      case 6:
+          femaleAkanName = "Ama";
+          break;
+    }
+    //alert(femaleAkanName);
+    document.getElementById("output").innerHTML = "Your Akan Name is "+" "+femaleAkanName;
+  }
     if (male.checked == true){
       var maleAkanName;
       switch(dDay){
         case 0:
-            maleAkanName = 'Kwasi';
-            break;
+          maleAkanName = 'Kwasi';
+          break;
         case 1:
-            maleAkanName = "Kwadwo";
-            break;
+          maleAkanName = "Kwadwo";
+          break;
         case 2:
-            maleAkanName = "Kwabena";
-            break;
+          maleAkanName = "Kwabena";
+          break;
         case 3:
-            maleAkanName = "Kwaku";
-            break;
+          maleAkanName = "Kwaku";
+          break;
         case 4:
-            maleAkanName = "Yaw";
-            break;
+          maleAkanName = "Yaw";
+          break;
         case 5:
-            maleAkanName = "Kofi";
+          maleAkanName = "Kofi";
+          break;
         case 6:
-            maleAkanName = "Kwame";
-            break;
+          maleAkanName = "Kwame";
+          break;
       }
       //alert(maleAkanName);
       document.getElementById('output').innerHTML ="Your Akan Name is "+" " + maleAkanName;
     }
   }
-  birthday ()
+
+
+  generateName()
 }
